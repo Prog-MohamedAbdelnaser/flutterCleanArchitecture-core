7cf1edc104ae2d1be44a8f4e977ffd225d3a7a25
lenovo <mohamedabdelnaser782@gmail.com>
2023-12-29 17:48:29 +0200

    init

--- lib/src/softMaterials.dart
+++ lib/src/softMaterials.dart
@@ -7,5 +7,7 @@ export 'core/managers/dialogs_manager.dart';
 export 'core/components/placeholder/error_placeholder_widget.dart';
 export 'core/components/loading_widget.dart';
 export 'core/components/restart_app_widget.dart';
-
+export 'core/bloc/basebloc.dart';
+export 'core/managers/errorManager.dart';
+export 'core/managers/dialogs_manager.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
