7cd8cac6fd7077f2eaffcf5770f0cf20d32f3bfc
lenovo <mohamedabdelnaser782@gmail.com>
2024-01-06 16:13:03 +0200

    init

--- lib/src/core/components/bottom_navigation_widget.dart
+++ lib/src/core/components/bottom_navigation_widget.dart
@@ -6,16 +6,16 @@ class BottomNavigationWidget extends StatefulWidget {
   final List<BottomNavigationWidgetModel> items;
   final AppBar? appBar;
   final Color? backgroundColor;
-
   final Color? barColor;
   final double? barElevation;
-
+  final EdgeInsetsGeometry?  itemsPadding ;
   final Widget Function(BuildContext context, Widget bar)? builder;
 
   const BottomNavigationWidget(
       {Key? key,
       required this.items,
       this.appBar,
+      this.itemsPadding,
       this.backgroundColor,
       this.barColor,
       this.builder,
@@ -72,7 +72,7 @@ class BottomNavigationState extends State<BottomNavigationWidget> {
           ?.copyWith(color: context.theme.unselectedWidgetColor),
       items: widget.items
           .map((e) =>
-              e.builder(_selectedIndex == widget.items.indexOf(e), context))
+              e.builder(_selectedIndex == widget.items.indexOf(e), context,widget.itemsPadding))
           .toList(),
       currentIndex: _selectedIndex,
       selectedItemColor: context.theme.primaryColor,
@@ -85,50 +85,50 @@ class BottomNavigationWidgetModel {
   final Widget widget;
   final String label;
   final String? iconPath;
-  final Widget? icon;
+  final double? iconSize;
+  
+  
+  final Widget Function(bool selected)? iconBuilder;
 
   BottomNavigationWidgetModel(
-      {required this.widget, required this.label, this.iconPath, this.icon})
-      : assert(icon == null || iconPath == null,
-            'Cannot provide both a icon and a icon');
+      {required this.widget, required this.label, this.iconPath, this.iconBuilder,this.iconSize})
+      : assert(iconBuilder == null || iconPath == null,
+            'Cannot provide both a iconBuilder and a icon');
 
-  BottomNavigationItemBuilder builder(bool selected, BuildContext context) {
+  BottomNavigationItemBuilder builder(bool selected, BuildContext context,EdgeInsetsGeometry ? itemsPadding) {
     return BottomNavigationItemBuilder(
       this,
       context,
       selected,
+      itemsPadding
     );
   }
 }
 
 class BottomNavigationItemBuilder extends BottomNavigationBarItem {
   BottomNavigationItemBuilder(
-      BottomNavigationWidgetModel model, BuildContext context,
-      [bool? selected])
+      BottomNavigationWidgetModel model, BuildContext context, [bool? selected,EdgeInsetsGeometry ? itemsPadding])
       : super(
-            icon: Column(
-              children: [
-                const SizedBox(
-                  height: 12,
-                ),
-                model.icon != null
-                    ? model.icon!
-                    : model.iconPath!.endsWith('svg') == true
-                        ? SvgPicture.asset(
-                            model.iconPath!,
-                            height: 24,
-                            colorFilter: selected == true
-                                ? ColorFilter.mode(
-                                    context.theme.primaryColor, BlendMode.color)
-                                : ColorFilter.mode(
-                                    context.theme.unselectedWidgetColor,
-                                    BlendMode.color),
-                          )
-                        : Image.asset(model.iconPath!),
-                const SizedBox(
-                  height: 4,
-                )
-              ],
+            icon: Padding(
+              padding: itemsPadding?? EdgeInsets.all(8),
+              child: _buildItem(model,selected==true,context),
             ),
             label: model.label);
+
+ static Widget _buildItem( BottomNavigationWidgetModel model,bool selected,BuildContext context) {
+    return model.iconBuilder != null
+        ? model.iconBuilder!(selected==true)
+        : model.iconPath!.endsWith('svg') == true
+        ? SvgPicture.asset(
+      model.iconPath!,
+      height: model.iconSize??24,
+      colorFilter: selected == true
+          ? ColorFilter.mode(
+          context.theme.primaryColor, BlendMode.color)
+          : ColorFilter.mode(
+          context.theme.unselectedWidgetColor,
+          BlendMode.color),
+    )
+        : Image.asset(model.iconPath!);
+  }
 }
