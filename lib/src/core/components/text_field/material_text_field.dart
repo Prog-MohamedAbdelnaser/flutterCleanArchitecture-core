64e77bebc48ed2086525288bc10ab3df6d74f097
lenovo <mohamedabdelnaser782@gmail.com>
2024-01-27 18:50:55 +0200

    init

--- lib/src/core/components/text_field/material_text_field.dart
+++ lib/src/core/components/text_field/material_text_field.dart
@@ -4,6 +4,9 @@ import '../../../../main_index.dart';
 
 class MaterialTextField extends StatelessWidget {
   final String? title;
+  final String? hintText;
+  final String? label;
+
   final TextEditingController? controller ;
   final EdgeInsetsGeometry? margin ;
    final AutovalidateMode? autovalidateMode ;
@@ -23,7 +26,7 @@ class MaterialTextField extends StatelessWidget {
   final TextInputAction? textInputAction ;
   final TextInputType ?keyboardType ;
   final ValueChanged<String>? onChanged;
-  const MaterialTextField({Key? key,this.style ,  this.controller, this.margin, this.autovalidateMode= AutovalidateMode.onUserInteraction, this.validator,
+  const MaterialTextField({Key? key,this.style ,this.hintText,this.label,  this.controller, this.margin, this.autovalidateMode= AutovalidateMode.onUserInteraction, this.validator,
     this.focusNode, this.inputDecoration, this.keyboardType, this.onChanged, this.radius = 8, this.initialValue, this.enabled,
     this.color , this.obscureText, this.textInputAction = TextInputAction.next,this.startIcon, this.onTap, this.readOnly =false, this.maxLines=1, this.title}) : super(key: key);
 
@@ -62,7 +65,7 @@ class MaterialTextField extends StatelessWidget {
                   obscureText: obscureText==true,
                   onChanged:onChanged,
                   keyboardType: keyboardType,
-                  decoration: inputDecoration!=null ? inputDecoration! :context.inputDecoration().copyWith(label: Text('data')),
+                  decoration: inputDecoration!=null ? inputDecoration! :context.inputDecoration().copyWith(label:label!=null ? Text(label!):SizedBox.shrink(),hintText: hintText),
                 ),
               ),
             ],
