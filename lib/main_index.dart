bb35e8b206b6d761f18466cff7ed063dd207d7f7
lenovo <mohamedabdelnaser782@gmail.com>
2024-02-16 18:31:01 +0200

    init

--- lib/main_index.dart
+++ lib/main_index.dart
@@ -4,6 +4,8 @@ export 'src/core/bloc/data_state.dart';
 export 'src/core/managers/dialogs_manager.dart';
 export 'src/core/components/placeholder/error_placeholder_widget.dart';
 export 'src/core/components/loading_widget.dart';
+export 'src/core/components/list/multi_selectable_list_widget.dart';
+export 'src/core/components/list/selectable_list_widget.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:flutter/material.dart';
